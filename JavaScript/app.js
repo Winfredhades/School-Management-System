3ccee2c927245a26a01468877c49d46692af5779
Muhammed Enes Eyibilen <muhammedeneseyibilen@Muhammeds-MacBook-Air.local>
2023-12-08 16:37:25 +0100

    I have Started to create functions and tested some of them are working not fully correctly.

---
+++ JavaScript/app.js
@@ -0,0 +1,56 @@
+import { students } from "../Data/studentData.js";
+
+// set data to local storage
+
+const studentDataRenderer = () => {
+    const studentInfo = document.getElementsByClassName('studentAdd');
+    const studentData = {
+        name: studentInfo[0].value,
+        course: studentInfo[1].value,
+        message: studentInfo[2].value,
+        };
+        localStorage.setItem("studdentInfo", JSON.stringify(studentData));
+}
+
+// get data from local storage
+
+const getDataFromLocalStorage = () => {
+    const studentData = JSON.parse(localStorage.getItem("studdentInfo"));
+    students.push(studentData);
+    return studentData;
+}
+
+// Create New Cart;
+
+const cardCreator = () => {
+    const card = document.getElementById('studentCartPart');
+    const studentInfo = getDataFromLocalStorage();
+    const studentCardText = `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body text-start">
+      <h5 class="card-title">${studentInfo.name}</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">${studentInfo.course}</h6><br>
+      <p class="card-text">${studentInfo.message}</p> <br>
+      <p class="text-primary">Average Grade: </p>  
+    </div>
+  </div>`
+  card.innerHTML = studentCardText;
+
+}
+
+const saveBtn = document.getElementById('studentSaveBtn');
+
+saveBtn.addEventListener("click", function() {
+   studentDataRenderer();
+   getDataFromLocalStorage();
+   cardCreator()
+   localStorage.clear()
+})
+
+
+
+
+
+
+
+
